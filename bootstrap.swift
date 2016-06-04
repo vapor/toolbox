8c8da131e9f4503d4e75b53a47294bf9e5bd1ff1
Sven A. Schmidt <sas@feinstruktur.com>
2016-06-04 10:15:59 +0100

    Refactored a bit

--- bootstrap.swift
+++ bootstrap.swift
@@ -81,49 +81,62 @@ func build(directory: String) throws {
     try run(cmd)
 }
 
-let url = "https://github.com/qutheory/vapor-cli/archive/spm.tar.gz"
-let archive = "./tmp.tgz"
-let unpackedDir = "./vapor-cli-spm"
 
-do {
-    if !exists(path: archive) {
-        print("Downloading \(url) ...")
-        try curl(url: url, output: archive, verbose: true)
+func downloadURL(repository: String, branch: String = "master") -> String {
+    return "https://github.com/qutheory/\(repository)/archive/\(branch).tar.gz"
+}
+
+
+func bootstrap(repository: String, branch: String) {
+    let url = downloadURL(repository: repository, branch: branch)
+    let archive = "./tmp.tgz"
+    // this directory name is dermined by how github creates the tar.gz
+    let unpackedDir = "./\(repository)-\(branch)"
+
+    do {
+        if !exists(path: archive) {
+            print("Downloading \(url) ...")
+            try curl(url: url, output: archive, verbose: true)
+        }
+    } catch {
+        fail("Could not download SPM package")
     }
-} catch {
-    fail("Could not download SPM package")
-}
 
-do {
-    print("Unpacking \(archive) ...")
-    try unpack(archive: archive, verbose: false)
-} catch {
-    fail("Could not unpack archive")
-}
-
-do {
-    print("Building package ...")
-    try build(directory: unpackedDir)
-} catch {
-    fail("Could not build package")
-}
-
-print("Cleaning up ...")
-
-do {
-    if exists(path: unpackedDir) {
-        try rm(directory: unpackedDir)
+    do {
+        print("Unpacking \(archive) ...")
+        try unpack(archive: archive, verbose: false)
+    } catch {
+        fail("Could not unpack archive")
     }
-} catch {
-    fail("Could not remove directory '\(unpackedDir)'")
-}
 
-do {
-    if exists(path: archive) {
-        try rm(file: archive)
+    do {
+        print("Building package ...")
+        try build(directory: unpackedDir)
+    } catch {
+        fail("Could not build package")
     }
-} catch {
-    fail("Could not remove archive '\(archive)'")
+
+    print("Cleaning up ...")
+
+    do {
+        if exists(path: unpackedDir) {
+            try rm(directory: unpackedDir)
+        }
+    } catch {
+        fail("Could not remove directory '\(unpackedDir)'")
+    }
+    
+    do {
+        if exists(path: archive) {
+            try rm(file: archive)
+        }
+    } catch {
+        fail("Could not remove archive '\(archive)'")
+    }
+    
+    print("Done.")
 }
 
-print("Done.")
+// main
+
+bootstrap(repository: "vapor-cli", branch: "spm")
