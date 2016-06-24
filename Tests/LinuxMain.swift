5e79cc07cb3111c65e705aeb84c4f97d6f64ed7b
Sven A. Schmidt <sas@finestructure.co>
2016-06-24 11:25:35 +0100

    Added sequence test

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,4 +4,5 @@ import XCTest
 XCTMain([
     testCase(ArrayExtTests.allTests),
     testCase(CmdDockerTests.allTests),
+    testCase(SequenceExt.allTests),
 ])
---
+++ Tests/VaporCLI/SequenceExt.swift
@@ -0,0 +1,27 @@
+//
+//  SequenceExt.swift
+//  VaporCLI
+//
+//  Created by Sven A. Schmidt on 24/06/2016.
+//
+//
+
+import XCTest
+@testable import VaporCLI
+
+class SequenceExt: XCTestCase {
+
+    // required by LinuxMain.swift
+    static var allTests: [(String, (SequenceExt) -> () throws -> Void)] {
+        return [
+            ("test_valueFor", test_valueFor),
+        ]
+    }
+
+    func test_valueFor() {
+        let args = ["--name=foo", "--debug"]
+        XCTAssertEqual(args.valueFor(argument: "name"), Optional("foo"))
+        XCTAssertNil(args.valueFor(argument: "debug"))
+    }
+
+}
