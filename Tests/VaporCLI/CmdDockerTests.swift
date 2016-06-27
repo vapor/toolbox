ad1b22fd6b6a60489c1013b012ec6aade92df1c6
Sven A. Schmidt <sas@finestructure.co>
2016-06-27 14:44:25 +0100

    Remaining tests for `docker init` subcommand

--- Tests/VaporCLI/CmdDockerTests.swift
+++ Tests/VaporCLI/CmdDockerTests.swift
@@ -20,9 +20,11 @@ class CmdDockerTests: XCTestCase {
     static var allTests: [(String, (CmdDockerTests) -> () throws -> Void)] {
         return [
             ("test_subCommands", test_subCommands),
-            ("test_execute_init", test_execute_init),
-            ("test_execute_init_verbose", test_execute_init_verbose),
-            ("test_execute_init_Dockerfile_exists", test_execute_init_Dockerfile_exists),
+            ("test_init", test_init),
+            ("test_init_verbose", test_init_verbose),
+            ("test_init_Dockerfile_exists", test_init_Dockerfile_exists),
+            ("test_init_download_failure", test_init_download_failure),
+            ("test_init_help", test_init_help),
         ]
     }
 
@@ -32,27 +34,50 @@ class CmdDockerTests: XCTestCase {
         shell = TestShell(logEvent: { log.append($0) })
     }
 
+    // MARK: Tests - General
+
     func test_subCommands() {
         let expected: [Command.Type] = [Docker.Init.self, Docker.Build.self, Docker.Run.self, Docker.Enter.self]
         // map to String, because currently XCTAssert cannot compare Command.Type (doesn't find a suitable overload)
         XCTAssertEqual(Docker.subCommands.map {"\($0)"}, expected.map {"\($0)"})
     }
 
-    func test_execute_init() {
+    // MARK: Init subcommmand
+
+    func test_init() {
         Docker.execute(with: ["init"], in: "", shell: shell)
         let expected: [LogEntry] = [.ok("curl -L -s docker.qutheory.io -o Dockerfile")]
         XCTAssertEqual(log, expected)
     }
 
-    func test_execute_init_verbose() {
+    func test_init_verbose() {
         Docker.execute(with: ["init", "--verbose"], in: "", shell: shell)
         XCTAssertEqual(log, [.ok("curl -L  docker.qutheory.io -o Dockerfile")])
     }
 
-    func test_execute_init_Dockerfile_exists() {
+    func test_init_Dockerfile_exists() {
         shell.fileExists = true
         Docker.execute(with: ["init"], in: "", shell: shell)
         XCTAssertEqual(log, [.failed("A Dockerfile already exists in the current directory.\nPlease move it and try again or run `vapor docker build`.")])
     }
 
+    func test_init_download_failure() {
+        shell.commandResults = { cmd in
+            if cmd.hasPrefix("curl") {
+                // fake a "Failed to connect to host" error
+                return .error(7)
+            } else {
+                return .ok(cmd)
+            }
+        }
+        Docker.execute(with: ["init"], in: "", shell: shell)
+        XCTAssertEqual(log, [
+            .error(7),
+            .failed("Could not download Dockerfile.")
+            ])
+    }
+
+    func test_init_help() {
+        XCTAssert(Docker.Init.help.count > 0)
+    }
 }
