cd0c0cf9e06415e14bc1ea7e17905a6ed69823a6
Sven A. Schmidt <sas@finestructure.co>
2016-06-30 08:22:52 +0100

    Applied fixit

--- Sources/VaporCLI/VaporCLI.swift
+++ Sources/VaporCLI/VaporCLI.swift
@@ -4,18 +4,18 @@ public struct VaporCLI {
     // this closure assignment is necessary to be able to exclude Xcode on Linux
     public static let commands: [Command.Type] = {
         var c = [Command.Type]()
-        c.append(Help)
-        c.append(Version)
-        c.append(Clean)
-        c.append(Build)
-        c.append(Run)
-        c.append(New)
-        c.append(Update)
+        c.append(Help.self)
+        c.append(Version.self)
+        c.append(Clean.self)
+        c.append(Build.self)
+        c.append(Run.self)
+        c.append(New.self)
+        c.append(Update.self)
         #if os(OSX)
-            c.append(Xcode)
+            c.append(Xcode.self)
         #endif
-        c.append(Heroku)
-        c.append(Docker)
+        c.append(Heroku.self)
+        c.append(Docker.self)
         return c
     }()
 }
