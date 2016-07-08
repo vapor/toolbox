14e11ea60b064a67ffccb127063fa473e6283e6b
Tanner Nelson <me@tanner.xyz>
2016-07-08 18:31:05 -0400

    semicolons

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
 
-TAG="0.6.0"
-SWIFT_VERSION="swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a"
+TAG="0.6.0";
+SWIFT_VERSION="swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a";
 
-SWIFT=`which swift`
+SWIFT=`which swift`;
 
 if [ $SWIFT == *"swiftenv"* ];
 then
-	echo "Swiftenv installed, continuing..."
-	swiftenv install
+	echo "Swiftenv installed, continuing...";
+	swiftenv install;
 else
 	if [ $SWIFT == *"swift-latest.xctoolchain"* ];
 	then
-		PATH=`ls -lah /Library/Developer/Toolchains/swift-latest.xctoolchain`
+		PATH=`ls -lah /Library/Developer/Toolchains/swift-latest.xctoolchain`;
 		if [ $PATH == *$SWIFT_VERSION* ];
 		then
-			echo "$SWIFT_VERSION installed, continuing..."
+			echo "$SWIFT_VERSION installed, continuing...";
 		else
-			echo "Incorrect Swift version installed, please install $SWIFT_VERSION"
-			exit 1
+			echo "Incorrect Swift version installed, please install $SWIFT_VERSION";
+			exit 1;
 		fi
 	else
 		if [ $SWIFT == *$SWIFT_VERSION* ];
 		then
-			echo "$SWIFT_VERSION installed, continuing..."
+			echo "$SWIFT_VERSION installed, continuing...";
 		else
-			echo "Neither Swift nor Swiftenv is installed"
-			echo "Please review the documentation for installing the Toolbox at http://docs.qutheory.io"
-			exit 1
+			echo "Neither Swift nor Swiftenv is installed";
+			echo "Please review the documentation for installing the Toolbox at http://docs.qutheory.io";
+			exit 1;
 		fi
 	fi
 fi
 
-TMP="vapor-toolbox-temp.zip"
-DIR="vapor-toolbox-$TAG"
+TMP="vapor-toolbox-temp.zip";
+DIR="vapor-toolbox-$TAG";
 
-rm -rf $TMP $DIR
+rm -rf $TMP $DIR;
 
-curl -L "https://github.com/qutheory/vapor-toolbox/archive/$TAG.zip" -o $TMP
-tar xf $TMP
-rm -rf $TMP
+curl -L "https://github.com/qutheory/vapor-toolbox/archive/$TAG.zip" -o $TMP;
+tar xf $TMP;
+rm -rf $TMP;
 
-cd $DIR
-swift build -c release
-.build/release/Executable self install
+cd $DIR;
+swift build -c release;
+.build/release/Executable self install;
 
-cd ..
-rm -rf $DIR
+cd ..;
+rm -rf $DIR;
