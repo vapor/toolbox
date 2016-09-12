03fd470cb4245febb646c5c2e5d4ff28ea764122
Logan Wright <logan.william.wright@gmail.com>
2016-09-11 20:08:45 -0400

    specify json

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     dependencies: [
         // Console protocols, terminal, and commands
         .Package(url: "https://github.com/vapor/console.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 0)
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 0, minor: 7)
     ]
 )
