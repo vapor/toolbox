91deee148346dcabb2b4b29d7e1dd03fe083b08f
Sergey Armodin <mak@arm1.ru>
2017-02-02 17:17:27 +0300

    fix for new line (compile error)

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ TAG=$(git describe --tags);
 git checkout $TAG > /dev/null 2>&1;
 
 cat Sources/Executable/main.swift | \
-    awk -v tag="$TAG" '/let version = "master"/ { printf "let version = \"%s\"\n", tag; next } 1' > .tmp && \
+    awk -v tag="$TAG" '/let version = "master"/ { printf "let version = \"%s\" \n ", tag; next } 1' > .tmp && \
     mv .tmp Sources/Executable/main.swift;
 
 echo "🛠  Compiling...";
