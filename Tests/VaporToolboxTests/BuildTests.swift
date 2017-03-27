2920cdf08ea89083ca742d66c7735deb0b4402ab
Logan Wright <logan.william.wright@gmail.com>
2017-03-27 08:29:54 +0200

    tests for 3.0.2

--- Tests/VaporToolboxTests/BuildTests.swift
+++ Tests/VaporToolboxTests/BuildTests.swift
@@ -15,16 +15,29 @@ class BuildTests: XCTestCase {
 
         do {
             try build.run(arguments: ["--modulemap=false"])
-            XCTAssertEqual(console.outputBuffer, [
-                "No .build folder, fetch may take a while...",
-                "Fetching Dependencies [Done]",
-                "Building Project [Done]"
-            ])
-            XCTAssertEqual(console.executeBuffer, [
-                "ls -a .",
-                "swift package --enable-prefetching fetch",
-                "swift build --enable-prefetching",
-            ])
+            #if swift(>=3.1)
+                XCTAssertEqual(console.outputBuffer, [
+                    "No .build folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]"
+                ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "ls -a .",
+                    "swift package --enable-prefetching fetch",
+                    "swift build --enable-prefetching",
+                ])
+            #else
+                XCTAssertEqual(console.outputBuffer, [
+                    "No Packages folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]"
+                    ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "ls -a .",
+                    "swift package fetch",
+                    "swift build",
+                    ])
+            #endif
         } catch {
             XCTFail("Build run failed: \(error)")
         }
@@ -36,18 +49,35 @@ class BuildTests: XCTestCase {
 
         do {
             try build.run(arguments: ["--clean", "--modulemap=false"])
-            XCTAssertEqual(console.outputBuffer, [
-                "Cleaning [Done]",
-                "No .build folder, fetch may take a while...",
-                "Fetching Dependencies [Done]",
-                "Building Project [Done]"
-            ])
-            XCTAssertEqual(console.executeBuffer, [
-                "rm -rf Packages .build",
-                "ls -a .",
-                "swift package --enable-prefetching fetch",
-                "swift build --enable-prefetching",
-            ])
+
+            #if swift(>=3.1)
+                XCTAssertEqual(console.outputBuffer, [
+                    "Cleaning [Done]",
+                    "No .build folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]"
+                ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "rm -rf Packages .build",
+                    "ls -a .",
+                    "swift package --enable-prefetching fetch",
+                    "swift build --enable-prefetching",
+                ])
+            #else
+                XCTAssertEqual(console.outputBuffer, [
+                    "Cleaning [Done]",
+                    "No Packages folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]"
+                    ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "rm -rf Packages .build",
+                    "ls -a .",
+                    "swift package fetch",
+                    "swift build",
+                    ])
+
+            #endif
         } catch {
             XCTFail("Build run failed: \(error)")
         }
@@ -64,20 +94,37 @@ class BuildTests: XCTestCase {
 
         do {
             try build.run(arguments: ["--run", "--modulemap=false"])
-            XCTAssertEqual(console.outputBuffer, [
-                "No .build folder, fetch may take a while...",
-                "Fetching Dependencies [Done]",
-                "Building Project [Done]",
-                "Running \(name)..."
-            ])
-            XCTAssertEqual(console.executeBuffer, [
-                "ls -a .",
-                "swift package --enable-prefetching fetch",
-                "swift build --enable-prefetching",
-                "ls .build/debug",
-                "swift package dump-package",
-                ".build/debug/App"
-            ])
+            #if swift(>=3.1)
+                XCTAssertEqual(console.outputBuffer, [
+                    "No .build folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]",
+                    "Running \(name)..."
+                ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "ls -a .",
+                    "swift package --enable-prefetching fetch",
+                    "swift build --enable-prefetching",
+                    "ls .build/debug",
+                    "swift package dump-package",
+                    ".build/debug/App"
+                ])
+            #else
+                XCTAssertEqual(console.outputBuffer, [
+                    "No Packages folder, fetch may take a while...",
+                    "Fetching Dependencies [Done]",
+                    "Building Project [Done]",
+                    "Running \(name)..."
+                    ])
+                XCTAssertEqual(console.executeBuffer, [
+                    "ls -a .",
+                    "swift package fetch",
+                    "swift build",
+                    "ls .build/debug",
+                    "swift package dump-package",
+                    ".build/debug/App"
+                    ])
+            #endif
         } catch {
             XCTFail("Build run failed: \(error)")
         }
