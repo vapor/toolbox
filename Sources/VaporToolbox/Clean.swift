023f4ea469903d2d79d97a69987e260b6be3c855
Logan Wright <logan.william.wright@gmail.com>
2017-03-30 11:16:50 +0200

    update gets its own file

--- Sources/VaporToolbox/Clean.swift
+++ Sources/VaporToolbox/Clean.swift
@@ -43,37 +43,3 @@ public final class Clean: Command {
         cleanBar.finish()
     }
 }
-
-public final class Update: Command {
-    public let id = "update"
-
-    public let signature: [Argument] = [
-        Option(name: "xcode", help: ["Removes any Xcode projects while cleaning."])
-    ]
-
-    public let help: [String] = [
-        "Updates your dependencies."
-    ]
-
-    public let console: ConsoleProtocol
-
-    public init(console: ConsoleProtocol) {
-        self.console = console
-    }
-
-    public func run(arguments: [String]) throws {
-        let isVerbose = arguments.isVerbose
-        let bar = console.loadingBar(title: "Updating", animated: !isVerbose)
-        bar.start()
-        try console.execute(verbose: isVerbose, program: "swift", arguments: ["package", "update"])
-        bar.finish()
-
-        #if !os(Linux)
-            console.info("Changes to dependencies usually require Xcode to be regenerated.")
-            let shouldGenerateXcode = console.confirm("Would you like to regenerate your xcode project now?")
-            guard shouldGenerateXcode else { return }
-            let xcode = Xcode(console: console)
-            try xcode.run(arguments: arguments)
-        #endif
-    }
-}
