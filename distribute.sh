2f9b807a6afce4be2668d51b779ae648585fa3b0
Logan Wright <logan.william.wright@gmail.com>
2017-04-19 14:12:41 +0200

    fix distribute script to remove sqlite

--- distribute.sh
+++ distribute.sh
@@ -5,7 +5,8 @@ cat ./Sources/Executable/main.swift | \
     awk -v tag="$TAG" '/let version = "master"/ { printf "let version = \"%s\"\n", tag; next } 1' > .tmp && \
     mv .tmp Sources/Executable/main.swift;
 
-swift build -c release -Xswiftc -static-stdlib
+export FLUENT_NO_SQLITE=true
+swift build -c release -Xswiftc -static-stdlib -Xswiftc -DNO_SQLITE
 mv .build/release/Executable ./macOS-sierra
 
 echo "Drag and drop macOS-sierra into https://github.com/vapor/toolbox/releases/edit/$TAG"
