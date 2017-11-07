81c01a13351487b815e1c05a2d4654fa1a044736
Jonas Schwartz <jonas@qutheory.io>
2017-11-07 16:30:35 -0500

    Fixing redis host

--- Sources/Cloud/Globals.swift
+++ Sources/Cloud/Globals.swift
@@ -1,6 +1,5 @@
 #if Xcode || DEBUG
     let cloudURL = "http://0.0.0.0:8100"
 #else
-    let cloudURL = "http://0.0.0.0:8100"
-    //let cloudURL = "https://api.vapor.cloud"
+    let cloudURL = "https://api.vapor.cloud"
 #endif
