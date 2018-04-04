49a8fc248200a04086598623f9f1a8c38c491e56
Jonas Schwartz <jonas@qutheory.io>
2018-04-04 19:26:19 -0400

    Removing cloud beta

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -20,7 +20,6 @@ guard let executable = iterator.next() else {
 }
 
 let cloud = try Cloud.group(terminal)
-let cloudBeta = try Cloud.groupBeta(terminal)
 
 do {
     try terminal.run(executable: executable, commands: [
@@ -34,7 +33,6 @@ do {
         Xcode(console: terminal),
         Version(console: terminal, version: version),
         cloud,
-        cloudBeta,
         Group(id: "heroku", commands: [
             HerokuInit(console: terminal),
             HerokuPush(console: terminal),
