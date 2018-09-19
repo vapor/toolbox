3bd425b3691237ed27781f96db20589df9fd52ea
Zsolt Varadi <vzsg@users.noreply.github.com>
2018-09-20 00:04:40 +0200

    Remove harmful checkGitUpstream call from Update

--- Sources/VaporToolbox/Update.swift
+++ Sources/VaporToolbox/Update.swift
@@ -19,8 +19,6 @@ public final class Update: Command {
     }
 
     public func run(arguments: [String]) throws {
-        try checkGitUpstream()
-
         let isVerbose = arguments.isVerbose
         let bar = console.loadingBar(title: "Updating", animated: !isVerbose)
         bar.start()
@@ -34,37 +32,5 @@ public final class Update: Command {
             let xcode = Xcode(console: console)
             try xcode.run(arguments: arguments)
         #endif
-    }
-
-    func checkGitUpstream() throws {
-        guard gitInfo.isGitProject() else { return }
-        let currentBranch = try gitInfo.currentBranch()
-        
-        if let upstream = try? gitInfo.upstreamBranch() {
-            try gitInfo.verify(
-                local: currentBranch,
-                remote: upstream.remote,
-                upstream: upstream.branch
-            )
-        } else {
-            let remotes = try gitInfo.remoteNames()
-            let remote: String
-            if remotes.isEmpty {
-                return
-            } else if remotes.count == 1 {
-                remote = remotes[0]
-            } else if remotes.contains("origin") {
-                remote = "origin"
-            } else {
-                remote = try console.giveChoice(
-                    title: "Which remote are you tracking for '\(currentBranch)'?",
-                    in: remotes
-                )
-            }
-            try gitInfo.verify(
-                local: currentBranch,
-                remote: remote
-            )
-        }
     }
 }
