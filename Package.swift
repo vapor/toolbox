099a1bab321c5549ab4647fefac3424dbea600a3
Logan Wright <logan.william.wright@gmail.com>
2018-09-20 23:29:08 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,13 @@ import PackageDescription
 let package = Package(
     name: "VaporToolbox",
     dependencies: [
-//        // The Package Manager for the Swift Programming Language
-//        .package(url: "https://github.com/apple/swift-package-manager.git", .revision("swift-DEVELOPMENT-SNAPSHOT-2018-09-18-a")),
-
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "clibc"),
-        
         // All of the commands and logic that powers the Vapor toolbox
         .target(name: "VaporToolbox", dependencies: [
-//            "SwiftPM",
-            "clibc",
-            "Vapor"
+            "Vapor",
         ]),
 
         // Runnable module, executes the main command group.
