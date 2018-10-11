d08dd34b3480f9c01efe832f02e49d75ba4b6129
Logan Wright <logan.william.wright@gmail.com>
2018-10-11 15:55:21 +0200

    successfully posting ssh key

--- Sources/VaporToolbox/Cloud.swift
+++ Sources/VaporToolbox/Cloud.swift
@@ -1,11 +1,43 @@
 import Vapor
 
+/**
+
+ // MARK: User APIs
+
+ vapor cloud login
+ - email:
+ - pwd:
+
+ vapor cloud signup
+ - firstName:
+ - lastName:
+ - email:
+ - pwd:
+ - organizationName:
+
+ vapor cloud me
+
+ // MARK: SSH Key APIs
+
+ vapor cloud ssh create (-n name-of-key -p path-to-key)
+ // if one key, push
+
+ vapor cloud ssh list
+ // list all
+
+ vapor cloud ssh delete name-of-key
+ // if multiple names exist, ask which one
+
+ **/
+
 public func testCloud() throws {
     try signup()
 }
 
-let cloudBaseUrl = "https://api.v2.vapor.cloud/"
-let authUrl = cloudBaseUrl + "v2/auth/"
+let cloudBaseUrl = "https://api.v2.vapor.cloud/v2/"
+let gitUrl = cloudBaseUrl + "git"
+let gitSSHKeysUrl = gitUrl.finished(with: "/") + "keys"
+let authUrl = cloudBaseUrl + "auth/"
 let userUrl = authUrl + "users"
 let loginUrl = userUrl.finished(with: "/") + "login"
 let meUrl = userUrl.finished(with: "/") + "me"
@@ -67,8 +99,58 @@ struct UserApi {
         let headers = token.headers
         let response = client.send(.GET, headers: headers, to: meUrl)
         let resp = try response.wait()
-        print(resp)
         return try resp.content.decode(CloudUser.self).wait()
+    }
+}
+
+protocol API {
+    var token: Token { get }
+}
+
+struct SSHKey: Content {
+    let key: String
+    let name: String
+    let userID: UUID
+
+    let id: UUID
+    let createdAt: Date
+    let updatedAt: Date
+}
+
+extension API {
+    var contentHeaders: HTTPHeaders {
+        var head = token.headers
+        head.add(name: "Content-Type", value: "application/json")
+        return head
+    }
+}
+
+struct SSHKeyApi: API {
+    let token: Token
+
+    func push(name: String, path: String) throws {
+        guard FileManager.default.fileExists(atPath: path) else { throw "no rsa key found at \(path)" }
+        guard let file = FileManager.default.contents(atPath: path) else { throw "unable to load rsa key" }
+        guard let key = String(data: file, encoding: .utf8) else { throw "no string found in data" }
+        try push(name: name, key: key)
+    }
+
+    func push(name: String, key: String) throws {
+        struct Package: Content {
+            let name: String
+            let key: String
+        }
+        let package = Package(name: name, key: key)
+        let client = try makeClient()
+        let response = client.send(.POST, headers: contentHeaders, to: gitSSHKeysUrl) { try $0.content.encode(package) }
+        let resp = try response.wait()
+        print(resp)
+        print("")
+//        return try resp.content.decode(CloudUser.self).wait()
+    }
+
+    static func list() throws {
+
     }
 }
 
@@ -103,6 +185,10 @@ func signup() throws {
     print(token)
     let me = try UserApi.me(token: token)
     print(me)
+    print("")
+
+    let sshApi = SSHKeyApi(token: token)
+    try sshApi.push(name: "my-key", key: "this is not a real key, it's pretend")
     print("")
 //    let response = try client.post(signUpUrl, headers: HTTPHeaders(), content: new).wait()
 //    print(response)
