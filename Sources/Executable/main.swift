ee03a025bab70cf21aa62cbe5de721193b2a923b
Logan Wright <logan.william.wright@gmail.com>
2018-10-16 15:38:39 +0200

    quick addition base class to prevent constant usage logs

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -2,13 +2,5 @@ import Foundation
 import VaporToolbox
 import Vapor
 
-//try testCloud()
-
-do {
-    let app = try boot().wait()
-    try app.run()
-} catch {
-    let term = Terminal()
-    term.output("Error:", style: .error)
-    term.output("\(error)".consoleText())
-}
+let app = try boot().wait()
+try app.run()
--- Sources/VaporToolbox/TestCommand.swift
+++ Sources/VaporToolbox/TestCommand.swift
@@ -1,7 +1,31 @@
 import Vapor
 
 /// Cleans temporary files created by Xcode and SPM.
-struct Test: Command {
+//struct Test: Command {
+//    /// See `Command`.
+//    var arguments: [CommandArgument] = []
+//
+//    /// See `Command`.
+//    var options: [CommandOption] = []
+//
+//    /// See `Command`.
+//    var help: [String] = ["Quick tests."]
+//
+//    /// See `Command`.
+//    func run(using ctx: CommandContext) throws -> Future<Void> {
+//        print("ok")
+//        throw "bar"
+//        return .done(on: ctx.container)
+//    }
+//}
+
+class Test: BaseCommand {
+    override func trigger() throws {
+        throw "bar"
+    }
+}
+
+class BaseCommand: Command {
     /// See `Command`.
     var arguments: [CommandArgument] = []
 
@@ -11,8 +35,21 @@ struct Test: Command {
     /// See `Command`.
     var help: [String] = ["Quick tests."]
 
+    private(set) var ctx: CommandContext! = nil
+
     /// See `Command`.
     func run(using ctx: CommandContext) throws -> Future<Void> {
+        self.ctx = ctx
+        do {
+            try trigger()
+        } catch {
+            ctx.console.output("Error:", style: .error)
+            ctx.console.output("\(error)".consoleText())
+        }
         return .done(on: ctx.container)
+    }
+
+    open func trigger() throws {
+        throw "throwing up this errrrr"
     }
 }
