86c13b37d9e9c456d3c18a55218a95d5d3f5a9b7
Logan Wright <logan.william.wright@gmail.com>
2018-10-23 21:24:15 +0200

    starting some deploy work

--- Sources/VaporToolbox/CloudDeploy.swift
+++ Sources/VaporToolbox/CloudDeploy.swift
@@ -5,10 +5,7 @@ struct CloudDeploy: MyCommand {
     var arguments: [CommandArgument] = []
 
     /// See `Command`.
-    var options: [CommandOption] = [
-//        .value(name: "email", short: "e", default: nil, help: ["the email to use when logging in"]),
-//        .value(name: "password", short: "p", default: nil, help: ["the password to use when logging in"])
-    ]
+    var options: [CommandOption] = []
 
     /// See `Command`.
     var help: [String] = ["Deploys a Vapory Project"]
@@ -26,10 +23,5 @@ struct CloudDeployRunner {
     func run() throws {
         let isClean = try Git.isClean()
         guard isClean else { throw "Git status is dirty, please commit changes before continuing" }
-
-        print("foo")
-
-        try asdfasdf()
-        print("")
     }
 }
