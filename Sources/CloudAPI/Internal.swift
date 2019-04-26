04cca5599b8f6fc1b697e144813649287550d0ce
logan wri <logan.william.wright@gmail.com>
2019-04-26 14:38:58 +0200

    adding more cloud support and client

--- Sources/CloudAPI/Internal.swift
+++ Sources/CloudAPI/Internal.swift
@@ -16,6 +16,19 @@ private struct ResponseError: Content {
     let reason: String
 }
 
+extension EventLoopFuture {
+    public func _flatMap<NewValue>(file: StaticString = #file, line: UInt = #line, _ callback: @escaping (Value) throws -> EventLoopFuture<NewValue>) -> EventLoopFuture<NewValue> {
+        let wrapped: (Value) -> EventLoopFuture<NewValue> = { [unowned self] in
+            do {
+                return try callback($0)
+            } catch {
+                return self.eventLoop.makeFailedFuture(error)
+            }
+        }
+        return flatMap(wrapped)
+    }
+}
+
 extension EventLoopFuture where Value == ClientResponse {
 
     /// Clear the response
@@ -34,8 +47,7 @@ extension EventLoopFuture where Value == ClientResponse {
     /// contents might be A or B and need to move on
     /// will think about and revisit
     internal func become<C: Content>(_ type: C.Type) -> EventLoopFuture<C> {
-        todo()
-//        return validate().flatMap { try $0.content.decode(C.self) }
+        return validate().flatMapThrowing { try $0.content.decode(C.self) }
     }
 
     /// Use this to parse out error responses
@@ -47,7 +59,17 @@ extension EventLoopFuture where Value == ClientResponse {
     /// contents might be A or B and need to move on
     /// will think about and revisit
     internal func validate() -> EventLoopFuture<ClientResponse> {
-        todo()
+        return flatMapThrowing { response in
+            // Check if ErrorResponse (they return a successful 200, but are actually errors)
+            if let cloudError = try? response.content.decode(ResponseError.self), cloudError.error {
+                throw cloudError.reason
+            }
+            // if UNABLE to map ResponseError
+            // then the success response should be our object
+            // pass along response for subsequent testing
+            return response
+        }
+
 //        return flatMap { response in
 //            // Check if ErrorResponse (returns 200, but is error)
 //            let cloudError = try response.content.decode(ResponseError.self)
