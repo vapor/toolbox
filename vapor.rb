5ca17b9eb505a53661dadd98d5fee1cbca71b5da
Tanner Nelson <me@tanner.xyz>
2019-07-25 10:03:28 -0400

    add url

--- vapor.rb
+++ vapor.rb
@@ -1,7 +1,10 @@
 class Vapor < Formula
   desc "Vapor Toolbox"
   homepage "https://vapor.codes"
-  head "https://github.com/vapor/toolbox.git", :branch => "3"
+  # head "https://github.com/vapor/toolbox.git", :branch => "3"
+  url "https://github.com/vapor/toolbox/archive/3.1.11-test.tar.gz"
+  sha256 "4da68421b8bbcdfba95945b1b75c35a06683c20a10c601fc8981ef182d741557"
+  version "3.1.11"
   depends_on :xcode => "10"
   depends_on "libressl"
   depends_on "pkg-config"
