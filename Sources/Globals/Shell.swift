f769f253f4d2d6620d651b9412637ed7406aed19
Jonas Schwartz <jonas@josc.io>
2019-11-25 12:24:38 +0100

    Changing from dirs to pwd because of failure in Linux

--- Sources/Globals/Shell.swift
+++ Sources/Globals/Shell.swift
@@ -26,7 +26,7 @@ public struct Shell {
     }
 
     public static func cwd() throws -> String {
-        return try ProcessInfo.processInfo.environment["TEST_DIRECTORY"] ?? bash("dirs -l")
+        return try ProcessInfo.processInfo.environment["TEST_DIRECTORY"] ?? bash("pwd")
     }
 
     public static func allFiles(in dir: String? = nil) throws -> String {
