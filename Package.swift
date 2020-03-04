d62dfab95143841aa89f6ca16a84487bc8440cda
Tanner <tannernelson@gmail.com>
2020-03-04 14:56:23 -0500

    update to 5.2 + test ci (#305)

--- Package.swift
+++ Package.swift
@@ -1,27 +1,29 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "toolbox",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
     ],
     products: [
-        .executable(name: "vapor", targets: ["Executable"])
+        .executable(name: "vapor", targets: ["Executable"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .branch("master")),
-        .package(url: "https://github.com/jpsim/Yams.git", .branch("master")),
-        .package(url: "https://github.com/tanner0101/mustache.git", .branch("master")),
-        .package(url: "https://github.com/vapor/console-kit.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/tanner0101/mustache.git", from: "0.1.0"),
+        .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0-rc"),
     ],
     targets: [
         .target(name: "VaporToolbox", dependencies: [
-            "ConsoleKit",
-            "Mustache",
-            "NIO",
-            "Yams"
+            .product(name: "ConsoleKit", package: "console-kit"),
+            .product(name: "Mustache", package: "mustache"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "Yams", package: "Yams"),
         ]),
-        .target(name: "Executable", dependencies: ["VaporToolbox"]),
+        .target(name: "Executable", dependencies: [
+            .target(name: "VaporToolbox"),
+        ]),
     ]
 )
