6db7a4fc7eeb53fe8d7a44b1dd1fa05548a7cc1b
marzvrover <marzily@protonmail.com>
2020-12-29 03:03:22 -0500

    Add name_lower_kebab to context

--- Sources/VaporToolbox/New/TemplateScaffolder.swift
+++ Sources/VaporToolbox/New/TemplateScaffolder.swift
@@ -17,6 +17,7 @@ struct TemplateScaffolder {
         assert(destination.hasPrefix("/"))
         var context: [String: MustacheData] = [:]
         context["name"] = .string(name)
+        context["name_lower_kabab"] = .string(name.replacingOccurrences(of: " ", with: "-").lowercased())
         self.console.output(key: "name", value: name)
         for variable in self.manifest.variables {
             try self.ask(variable: variable, to: &context, using: &input)
