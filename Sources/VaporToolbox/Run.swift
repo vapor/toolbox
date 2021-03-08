6518e9d8631118126c8f0f75603438d08555994c
Thomas Krajacic <tkrajacic@users.noreply.github.com>
2021-03-08 12:22:59 +0100

    Don't try to build the app when using only the `-h` or `--help` flags on the `run` command (#344)
    * Don't try to build the app when using the `-h` flag on the `run` command
    
    Fixes #341
    
    * Improve legibility by using guard instead of if statement

--- Sources/VaporToolbox/Run.swift
+++ Sources/VaporToolbox/Run.swift
@@ -3,7 +3,7 @@ import Foundation
 
 // Generates an Xcode project
 struct Run: AnyCommand {
-    let help = "Runs an app from the console."
+    let help = "Runs an app from the console.\nEquivalent to `swift run --enable-test-discovery Run`."
 
     func run(using context: inout CommandContext) throws {
         var extraArguments: [String] = []
@@ -14,6 +14,11 @@ struct Run: AnyCommand {
     }
 
     func outputHelp(using context: inout CommandContext) {
+        guard context.input.arguments.count > 1 else {
+            context.console.output("\(self.help)")
+            return
+        }
+        
         do {
             context.input.arguments.append("--help")
             try self.run(using: &context)
