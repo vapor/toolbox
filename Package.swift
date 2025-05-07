125894c2047b1f5172c54b8e25b348ccc0f21d33
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-05-07 21:10:25 +0200

    Return to ArgumentParser v1.5.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "vapor", targets: ["VaporToolbox"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/swiftlang/swift-subprocess.git", branch: "main"),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.3.1"),
