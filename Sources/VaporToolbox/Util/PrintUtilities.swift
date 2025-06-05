99a27e61dd4bfd9910264135b6ce82fddccac2bc
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-06-05 17:53:54 +0200

    Remove superfluous `public` declaration

--- Sources/VaporToolbox/Util/PrintUtilities.swift
+++ Sources/VaporToolbox/Util/PrintUtilities.swift
@@ -95,7 +95,7 @@ extension Console {
     ///   - key: `String` to use as the key, which will precede the `value` an a colon.
     ///   - value: `String` to use as the value.
     ///   - style: ``ConsoleStyle`` to use for printing the `value`.
-    public func output(key: String, value: String, style: ConsoleStyle = .info) {
+    func output(key: String, value: String, style: ConsoleStyle = .info) {
         self.output(key.consoleText() + ": " + value.consoleText(style))
     }
 }
