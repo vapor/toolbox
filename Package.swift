fc576a4a72f13d8e8f3d2064d56cf1824427b6e9
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-11-21 17:12:03 +0100

    Set the correct dependencies versions

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.2"),
-        .package(url: "https://github.com/vapor/console-kit.git", branch: "5.0.0-alpha.1"),
-        .package(url: "https://github.com/swiftlang/swift-subprocess.git", from: "0.2.1"),
+        .package(url: "https://github.com/vapor/console-kit.git", exact: "5.0.0-alpha.1"),
+        .package(url: "https://github.com/swiftlang/swift-subprocess.git", .upToNextMinor(from: "0.2.1")),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.2"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "6.2.0"),
     ],
